4ca6dee2481ca5e8cefdb7bb62e5369d7873df23
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-11-16 14:37:06 -0800

    Refactor RootLayout to wrap QueryClientProvider with SafeAreaProvider for improved layout handling

--- PeerPrep/app/_layout.tsx
+++ PeerPrep/app/_layout.tsx
@@ -1,21 +1,23 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 // Create QueryClient instance
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PaperProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(app)" />
-        </Stack>
-      </PaperProvider>
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(app)" />
+          </Stack>
+        </PaperProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
